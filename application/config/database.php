339e68084b9a5d927f48c7347ae6a407aa75384c
jyyblue <jyyblue1987@outlook.com>
2020-01-27 16:18:42 +0200

    [add] change db setting

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'imocdbP@$$w0rd';
+$db['default']['password'] = '';
 $db['default']['database'] = 'onlinecamera_db';
 
 /*$db['default']['hostname'] = 'localhost';
